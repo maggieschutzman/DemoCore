4bbfaf6f0302c1f4408cd40a02cc8a65693fcabf
max-student@outlook.com <max-student@outlook.com>
2019-08-20 10:13:50 -0400

    Add project files.

---
+++ CoreDemo/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Models {
+
+    public class Customer {
+
+        [Key]
+        public int CustomerID { get; set; }
+
+        [MaxLength(30)] [Required]
+        public string Firstname { get; set; }
+
+        [MaxLength(50)] [Required]
+        public string Lastname { get; set; }
+
+        public DateTime Added { get; set; }
+
+        public decimal CreditLimit { get; set; }
+
+
+    }
+}
